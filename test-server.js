b0b8cc572fe6399ad2829d47657692ed6bf5a40c
Jeremie Miller <jeremie@jabber.org>
2014-03-17 20:14:25 -0600

    works in the browser too

--- test-server.js
+++ test-server.js
@@ -1,14 +1,15 @@
 var path = require("path");
 var th = require("telehash");
-th.debug(console.log);
-th.init({id:path.resolve("server.json"),seeds:path.resolve("seeds.json")},function(err,self){
+var seeds = require("./seeds.json");
+//th.debug(console.log);
+th.init({id:path.resolve("server.json"),seeds:seeds},function(err,self){
   if(err) return console.log(err);
   require("./index.js").install(self);
   self.thtp.listen(function(req,cbRes){
     console.log("got request",req.headers);
-//    cbRes(false,{status:200,body:"ok"});
+    cbRes({status:200,body:"ok"});
 //    cbRes().end("foobar");
-    process.stdin.pipe(cbRes());
+//    process.stdin.pipe(cbRes());
   });
   console.log("listening at thtp://"+self.hashname+"/")
 })
